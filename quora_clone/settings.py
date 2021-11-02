23c917123213df18c4970ba7b38f11aee292450c
Niladri Samanta <74195732+scavenOP@users.noreply.github.com>
2021-11-02 15:24:05 +0530

    Update settings.py

--- quora_clone/settings.py
+++ quora_clone/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[STATIC_DIR,]
+# STATICFILES_DIRS=[STATIC_DIR,]
 
 MESSAGE_TAGS = {
     messages.ERROR:'danger'
